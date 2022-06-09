5b7ee52d3fe6225a2ab8370b1d7a18cfa3d0a200
christian <christian.a.baird@gmail.com>
2022-06-08 23:47:59 -0400

    Adding script to version package

---
+++ version-package.js
@@ -0,0 +1,17 @@
+const sh = require('shelljs');
+
+module.exports = ((params) => {
+  const version = params.version;
+  const message = params.message;
+  const noGit = params.noGit;
+
+  let script = `npm version ${version} --allow-same-version -f`;
+  if (message) {
+    script += ` -m "${message}"`;
+  }
+  if (noGit) {
+    script += ` --no-git-tag-version`;
+  }
+
+  sh.exec(script);
+});
