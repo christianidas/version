e0df47e13d0c2e5e4e298a82bc68b3be8470ff58
christian <christian.a.baird@gmail.com>
2022-06-09 00:51:08 -0400

    Changing version-capacitor to just a helper for version ios script with default file location

--- version-capacitor.js
+++ version-capacitor.js
@@ -1,22 +1,9 @@
-const fs = require('fs');
-const sh = require('shelljs');
-const readPlistFile = require('simple-plist').readFileSync;
-const writePlistFile = require('simple-plist').writeFileSync;
-const iosInfoPlistPath = './ios/App/App/Info.plist';
+const versionIOS = require('./version-ios');
 
 module.exports = ((params) => {
-  const version = params.version;
-  const noGit = params.noGit;
-
-  if (fs.existsSync(iosInfoPlistPath)) {
-    const plistObject = readPlistFile(iosInfoPlistPath);
-
-    plistObject.CFBundleShortVersionString = version;
-    plistObject.CFBundleVersion = version;
-
-    writePlistFile(iosInfoPlistPath, plistObject);
-    if (!noGit) {
-      sh.exec(`git add ${iosInfoPlistPath}`);
-    }
-  }
+  const iosConfig = params.iosConfig || './ios/App/App/Info.plist';
+  return versionIOS({
+    ...params,
+    iosConfig,
+  })
 });
